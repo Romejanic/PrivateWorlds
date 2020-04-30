4d7882a27e04024d9f814f8c1bbb571ac89fdfdb
Jack Davenport <jack.davenport5011@gmail.com>
2020-04-30 18:01:06 +1000

    Created basic plugin framework

---
+++ src/com/romejanic/pw/PrivateWorlds.java
@@ -0,0 +1,20 @@
+package com.romejanic.pw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.romejanic.pw.utils.Constants;
+
+public class PrivateWorlds extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Started PrivateWorlds! Thanks for using the plugin!");
+		getLogger().info("If you like this plugin, give it a star on GitHub: " + Constants.GITHUB_URL);
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("Disabled PrivateWorlds! Thanks for using the plugin!");
+	}
+	
+}
---
+++ src/com/romejanic/pw/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.romejanic.pw.utils;
+
+public class Constants {
+
+	public static final String GITHUB_URL = "https://github.com/Romejanic/PrivateWorlds";
+	public static final String ISSUES_URL = "https://github.com/Romejanic/PrivateWorlds/issues/new/choose";
+	
+}
