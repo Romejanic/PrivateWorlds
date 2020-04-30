dcfe2b198d3ddaeb79376e5b5052690dec6cf45b
Jack Davenport <jack.davenport5011@gmail.com>
2020-04-30 18:19:25 +1000

    Added subcommand suggestions

--- src/com/romejanic/pw/cmd/BaseCommand.java
+++ src/com/romejanic/pw/cmd/BaseCommand.java
@@ -16,7 +16,17 @@ public class BaseCommand implements CommandExecutor {
 		if(args.length == 0 || args[0].equalsIgnoreCase(SUB_COMMANDS[0])) {
 			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " <" + Utils.join(SUB_COMMANDS, "|") + ">");
 		} else {
-			sender.sendMessage(Utils.join(args, " "));
+			String sub = args[0].toLowerCase();
+			switch(sub) {
+			default:
+				String closest = Utils.closestMatching(SUB_COMMANDS, sub);
+				sender.sendMessage(ChatColor.RED + "Unrecognised sub command: " + args[0]);
+				if(closest != null) {
+					sender.sendMessage(ChatColor.RED.toString() + ChatColor.ITALIC + "Did you mean: " + closest);
+				}
+				sender.sendMessage(ChatColor.RED + "Type " + ChatColor.BOLD + "/" + label + " help" + ChatColor.RED + " for a list of commands.");
+				break;
+			}
 		}
 		return true;
 	}
--- src/com/romejanic/pw/utils/Utils.java
+++ src/com/romejanic/pw/utils/Utils.java
@@ -2,6 +2,13 @@ package com.romejanic.pw.utils;
 
 public class Utils {
 
+	/**
+	 * Joins an array of strings together, with a given connective
+	 * string inserted between each of them.
+	 * @param arr The array to join
+	 * @param conn The connector to use
+	 * @return A string containing the joined elements of the array
+	 */
 	public static String join(String[] arr, String conn) {
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < arr.length; i++) {
@@ -11,6 +18,33 @@ public class Utils {
 			}
 		}
 		return sb.toString();
+	}
+	
+	/**
+	 * Tries to find the closest matching string in the array to the
+	 * string given. If no string matches by at least 50%, then no
+	 * string is returned.
+	 * @param arr The array of strings to test.
+	 * @param test The string to test against.
+	 * @return The string with at least 50% characters matching, or
+	 * null if no such string exists.
+	 */
+	public static String closestMatching(String[] arr, String test) {
+		String closest = null;
+		float percentage = 0f;
+		for(String s : arr) {
+			s = s.toLowerCase(); // ignore case
+			int count = 0;
+			for(int i = 0; i < s.length(); i++) {
+				if(test.contains(String.valueOf(s.charAt(i)))) count++;
+			}
+			float match = (float)count / s.length();
+			if(match > percentage) {
+				percentage = match;
+				closest = s;
+			}
+		}
+		return percentage < 0.5f ? null : closest;
 	}
 	
 }
